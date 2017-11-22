c3e831f0bc6274ea19e7c33472ab71b0f18bf36c
Gopal Ojha <gopalonevirus@gmail.com>
2017-11-22 07:56:55 +0545

    conversion

---
+++ StringtointerfaceConv.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+)
+
+func Display(vals []interface{}) {
+    for _, val := range vals {
+        fmt.Println(val)
+    }
+}
+
+func main() {
+    names := []string{"Gopal", "Mohan", "Bikesh"}
+   vals := make([]interface{}, len(names))
+    for i, v := range names {
+        vals[i] = v
+    }
+    Display(vals)
+}
